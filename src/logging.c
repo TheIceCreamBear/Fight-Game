68a13024437d5cfe9c515bf3cf8834f6b971103b
David Santamaria <davidsantamaria1245@gmail.com>
2021-05-06 15:59:22 -0400

    Test webhook integration #2

--- src/logging.c
+++ src/logging.c
@@ -4,6 +4,7 @@
 #include "logging.h"
 #include "colors.h"
 
+/* Log a message */
 void logs(const char *str) {
     if (!log_output_stream) {
         fprintf(stderr, "log_init() must be called before logs!\n");
@@ -12,6 +13,12 @@ void logs(const char *str) {
     fprintf(log_output_stream, "%s" ANSI_RESET "\n", str);
 }
 
+/* Manually set the output stream of log messages */
+void log_set_ouput_stream(FILE *fp) {
+    log_output_stream = fp;
+}
+
+/* Default-initialize the outputstream to stdout */
 void log_init(void) {
     log_output_stream = stdout; /* defualt output stream */
 }
