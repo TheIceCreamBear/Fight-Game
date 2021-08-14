4bd7c41b77e11d95743f01c5fa8626751ceb8127
David Santamaria <davidsantamaria1245@gmail.com>
2021-08-14 13:40:45 -0400

    small fixes

--- include/gl.h
+++ include/gl.h
@@ -10,17 +10,6 @@ typedef struct ShaderProgram {
     GLuint vertex;
 } ShaderProgram;
 
-const GLFWvidmode * setWindowedFullscreenHints(GLFWmonitor* mon) {
-    const GLFWvidmode* vidMode = glfwGetVideoMode(mon);
-
-    // set the window to use the current video mode
-    glfwWindowHint(GLFW_RED_BITS, vidMode->redBits);
-    glfwWindowHint(GLFW_GREEN_BITS, vidMode->greenBits);
-    glfwWindowHint(GLFW_BLUE_BITS, vidMode->blueBits);
-    glfwWindowHint(GLFW_REFRESH_RATE, vidMode->refreshRate);
-
-    // dont let it be resizeable
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-}
+const GLFWvidmode * setWindowedFullscreenHints(GLFWmonitor* mon);
 
 #endif
---
+++ src/gl.c
@@ -0,0 +1,14 @@
+#include "gl.h"
+
+const GLFWvidmode * setWindowedFullscreenHints(GLFWmonitor* mon) {
+    const GLFWvidmode* vidMode = glfwGetVideoMode(mon);
+
+    // set the window to use the current video mode
+    glfwWindowHint(GLFW_RED_BITS, vidMode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, vidMode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, vidMode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, vidMode->refreshRate);
+
+    // dont let it be resizeable
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+}
